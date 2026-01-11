140875070fcf69eaceda68722cbeee7bd1444780
orl0pl <93262125+orl0pl@users.noreply.github.com>
2026-01-11 11:04:58 +0100

    chore: updated readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # streak-api
-API for getting streak from Duolingo written with fastapi and deploed on Cloudflare Workers.
+Failed attempt at making an API for getting streak from Duolingo written with fastapi and deploed on Cloudflare Workers.
+Couldn't deploy it so I rewrote it in typescript
 
 This is my first API written in python.
+
+You can find working API repo here:
+https://github.com/orl0pl/streak-api
 
 ## How to Run
 
--- src/main.py
+++ src/main.py
@@ -19,23 +19,23 @@ app = FastAPI()
 async def read_root():
     return "API is running!"
 
-# @app.get("/user/{username}")
-# def read_user(username: str):
-#     response = fetch_userdata(username)
-#     if response.data:
-#         if len(response.data["users"]) > 0:
-#             return simplify_user_data(response.data["users"][0])
-#         else:
-#             return JSONResponse(status_code=404, content={"error": "User with username '" + username + "' not found"})
-#     return JSONResponse(status_code=500, content={"error": response.error})
+@app.get("/user/{username}")
+def read_user(username: str):
+    response = fetch_userdata(username)
+    if response.data:
+        if len(response.data["users"]) > 0:
+            return simplify_user_data(response.data["users"][0])
+        else:
+            return JSONResponse(status_code=404, content={"error": "User with username '" + username + "' not found"})
+    return JSONResponse(status_code=500, content={"error": response.error})
     
 
-# @app.get("/streak/{username}")
-# def read_streak(username: str):
-#     response = fetch_userdata(username)
-#     if response.data:
-#         if len(response.data["users"]) > 0:
-#             return get_user_streak(response.data["users"][0])
-#         else:
-#             return JSONResponse(status_code=404, content={"error": "User with username '" + username + "' not found"})
-#     return JSONResponse(status_code=500, content={"error": response.error})
+@app.get("/streak/{username}")
+def read_streak(username: str):
+    response = fetch_userdata(username)
+    if response.data:
+        if len(response.data["users"]) > 0:
+            return get_user_streak(response.data["users"][0])
+        else:
+            return JSONResponse(status_code=404, content={"error": "User with username '" + username + "' not found"})
+    return JSONResponse(status_code=500, content={"error": response.error})
